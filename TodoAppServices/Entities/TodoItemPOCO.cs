d6f84ce2436858c765d79e9f531b7b0de9523d11
jasonrdonald <jasonrdonald@yahoo.co.in>
2021-02-25 23:40:04 +0530

    Todo List app using .Net core web API, React

---
+++ TodoAppServices/Entities/TodoItemPOCO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoAppServices.Entities
+{
+    public static class TodoItemPOCO
+    {
+        public static List<TodoItem> Store { get; set; } = new List<TodoItem>() { new TodoItem { ID = 1, Task = "Task 1", IsComplete = false },
+                                                new TodoItem{ ID = 2, Task = "Task 2", IsComplete = false},
+                                                new TodoItem{ ID = 3, Task = "Task 3", IsComplete = true},
+                                                new TodoItem{ ID = 4, Task = "Task 4", IsComplete = true}
+                                                        };
+
+    }
+}
---
+++ TodoAppServices/Migrations/20210218110534_TodoMigration.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TodoAppServices.Migrations
+{
+    public partial class TodoMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TodoItem",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Task = table.Column<string>(nullable: true),
+                    IsComplete = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TodoItem", x => x.ID);
+                });
+
+            migrationBuilder.InsertData(
+                table: "TodoItem",
+                columns: new[] { "ID", "IsComplete", "Task" },
+                values: new object[] { 2, false, "Task 2" });
+
+            migrationBuilder.InsertData(
+                table: "TodoItem",
+                columns: new[] { "ID", "IsComplete", "Task" },
+                values: new object[] { 3, false, "Task 3" });
+
+            migrationBuilder.InsertData(
+                table: "TodoItem",
+                columns: new[] { "ID", "IsComplete", "Task" },
+                values: new object[] { 4, false, "Task 4" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TodoItem");
+        }
+    }
+}
