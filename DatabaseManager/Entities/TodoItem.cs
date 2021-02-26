a316664f9dbe67fcce6f559bcf9dbf2ffbb84964
jasonrdonald <jasonrdonald@yahoo.co.in>
2021-02-26 23:25:40 +0530

    Added DatabaseManager Project

---
+++ DatabaseManager/Entities/TodoItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DatabaseManager.Entities
+{
+    public class TodoItem
+    {
+        public int ID { get; set; }
+        public string Task { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
---
+++ DatabaseManager/Entities/TodoItemList.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DatabaseManager.Entities
+{
+    public class TodoItemList
+    {
+        public List<TodoItem> TodoItems { get; set; }
+    }
+}
